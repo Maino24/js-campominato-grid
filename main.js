94c51eb55759cb1a98d504f0d0270426933c2738
Maino24 <mg02.gabriele@gmail.com>
2022-10-28 17:59:21 +0200

    creato div griglia in html e div quadrato in java

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function genera(){
+let griglia = document.getElementById(`griglia`);
+//console.log(griglia)
+
+//function genera(){
     
+//}
+
+function creazioneQuadratino(){
+
+ const divQ = document.createElement(`div`);
+ divQ.classList.add(`quadrato`);
+ return divQ
 }
+
+console.log(creazioneQuadratino)
+
+//inseriamo il quadrato creato dalla funzione nella griglia
+griglia.append(creazioneQuadratino())
