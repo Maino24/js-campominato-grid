4ed1f8f638d98c302f3390dc2fe0b08f52ed5a73
Maino24 <mg02.gabriele@gmail.com>
2022-10-28 18:18:02 +0200

    creato ciclo for per inserire i 100 quadratini

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ function creazioneQuadratino(){
 
  const divQ = document.createElement(`div`);
  divQ.classList.add(`quadrato`);
+ 
  return divQ
 }
 
 console.log(creazioneQuadratino)
 
 //inseriamo il quadrato creato dalla funzione nella griglia
-griglia.append(creazioneQuadratino())
+//creo ciclo for che mi crea i 100 quadratini
+for(let i = 0; i < 100; i++){
+    griglia.append(creazioneQuadratino())
+}
