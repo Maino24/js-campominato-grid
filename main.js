67cb0dcf65599da778d341b3617d7b570357aae3
Maino24 <mg02.gabriele@gmail.com>
2022-10-31 17:03:57 +0100

    creati i tre bottoni per i 3 livelli

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-
+/* PRIMO LIVELLO*/
 document.getElementById(`generaLivelloUno`).onclick = function() {genera()}
   
 function genera(){
     document.getElementById(`generaLivelloUno`).style.display = "none";
+    document.getElementById(`generaLivelloDue`).style.display = "none";
+    document.getElementById(`generaLivelloTre`).style.display = "none";
 
 let griglia = document.getElementById(`griglia`);
 
@@ -51,4 +53,121 @@ for(let i = 0; i < 100; i++){
 
 }
 
+}
+
+
+/* SECONDO LIVELLO */
+
+document.getElementById(`generaLivelloDue`).onclick = function() {generaDue()}
+function generaDue(){
+    document.getElementById(`generaLivelloUno`).style.display = "none";
+    document.getElementById(`generaLivelloDue`).style.display = "none";
+    document.getElementById(`generaLivelloTre`).style.display = "none";
+
+    let griglia = document.getElementById(`griglia`);
+
+//console.log(griglia)
+
+//function genera(){
+    
+//}
+
+function creazioneQuadratino(){
+
+ const divQ = document.createElement(`div`);
+ divQ.classList.add(`quadrato`);
+ 
+ return divQ
+
+}
+console.log(creazioneQuadratino)
+
+//somma = 0
+
+
+//inseriamo il quadrato creato dalla funzione nella griglia
+//creo ciclo for che mi crea i 100 quadratini
+for(let i = 0; i < 100; i++){
+        let elemento = creazioneQuadratino()
+        console.log(elemento);
+        elemento.addEventListener(`click`, function(){
+            console.log(this);
+            this.classList.toggle(`attivazione`)
+            somma = i
+            somma = somma + 1
+           console.log(somma)
+           document.write(somma);
+
+           //elemento.classList.add(`numerazione`)
+    })
+    
+
+
+   /* elemento.addEventListener(`click`, function(){
+        console.log(this)
+        this.classList.toggle(`numerazione`)
+    })*/
+    griglia.append(elemento)
+
+}
+}
+
+
+/* TERZO LIVELLO */
+
+
+/* SECONDO LIVELLO */
+
+document.getElementById(`generaLivelloTre`).onclick = function() {generaTre()}
+function generaTre(){
+    document.getElementById(`generaLivelloUno`).style.display = "none";
+    document.getElementById(`generaLivelloDue`).style.display = "none";
+    document.getElementById(`generaLivelloTre`).style.display = "none";
+
+    let griglia = document.getElementById(`griglia`);
+
+//console.log(griglia)
+
+//function genera(){
+    
+//}
+
+function creazioneQuadratino(){
+
+ const divQ = document.createElement(`div`);
+ divQ.classList.add(`quadrato`);
+ 
+ return divQ
+
+}
+console.log(creazioneQuadratino)
+
+//somma = 0
+
+
+//inseriamo il quadrato creato dalla funzione nella griglia
+//creo ciclo for che mi crea i 100 quadratini
+for(let i = 0; i < 100; i++){
+        let elemento = creazioneQuadratino()
+        console.log(elemento);
+        elemento.addEventListener(`click`, function(){
+            console.log(this);
+            this.classList.toggle(`attivazione`)
+            somma = i
+            somma = somma + 1
+           console.log(somma)
+           document.write(somma);
+
+           //elemento.classList.add(`numerazione`)
+    })
+    
+
+
+   /* elemento.addEventListener(`click`, function(){
+        console.log(this)
+        this.classList.toggle(`numerazione`)
+    })*/
+    griglia.append(elemento)
+
+}
 }
