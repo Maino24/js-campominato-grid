64f88c6336070d1557df6d0271b061d7387c5b05
Maino24 <mg02.gabriele@gmail.com>
2022-11-01 10:14:23 +0100

    select option

--- main.js
+++ main.js
@@ -1,9 +1,162 @@
 document.getElementById(`griglia`).style.display = "none";
+let button = document.getElementById(`paly`)
+let inputLivello = document.getElementById(`generaLivelloUNo`)
+let inputLivelloUno = document.getElementById(`generaLivelloUno`) 
+let inputLivelloDue = document.getElementById(`generaLivelloDue`) 
+let inputLivelloTre = document.getElementById(`generaLivelloTre`) 
+
+
+
+button.addEventListener(`click`, function(){
+//document.getElementById(`play`).onclick = function() {genera()}
+//function genera(){
+//const valueLivello = inputLivello.value;
+//const valueLivelloUno = inputLivelloUno.value;
+//const valueLivelloDue = inputLivelloDue.value;
+//const valueLivelloTre = inputLivelloTre.value;
+//console.log(valueLivello)
+
+//}
+if(inputLivello = `uno`){
+    function genera(){
+        document.getElementById(`generaLivelloUno`).style.display = "none";
+        //document.getElementById(`generaLivelloDue`).style.display = "none";
+        //document.getElementById(`generaLivelloTre`).style.display = "none";
+        document.getElementById(`titolo`).style.display = "none";
+        document.getElementById(`griglia`).style.display = "flex";
+    let griglia = document.getElementById(`griglia`);
+    
+    function creazioneQuadratino(){
+    
+     const divQ = document.createElement(`div`);
+     divQ.classList.add(`quadrato`);
+     
+     return divQ
+    
+    }
+    console.log(creazioneQuadratino)
+    
+    
+    
+    
+    //inseriamo il quadrato creato dalla funzione nella griglia
+    //creo ciclo for che mi crea i 100 quadratini
+    for(let i = 0; i < 100; i++){
+            let elemento = creazioneQuadratino()
+            console.log(elemento);
+            elemento.addEventListener(`click`, function(){
+                console.log(this);
+                this.classList.toggle(`attivazione`)
+                somma = i
+                somma = somma + 1
+               console.log(somma)
+               
+        })
+        
+    
+    
+       
+        griglia.append(elemento)
+    
+    }
+    
+    }
+}else if(inputLivello = inputLivelloDue){
+
+    function generaDue(){
+        document.getElementById(`generaLivelloUno`).style.display = "none";
+        document.getElementById(`generaLivelloDue`).style.display = "none";
+        document.getElementById(`generaLivelloTre`).style.display = "none";
+        document.getElementById(`titolo`).style.display = "none";
+        document.getElementById(`griglia`).style.display = "flex";
+        let griglia = document.getElementById(`griglia`);
+    
+    
+    
+    function creazioneQuadratino(){
+    
+     const divQ = document.createElement(`div`);
+     divQ.classList.add(`quadratoDue`);
+     
+     return divQ
+    
+    }
+    console.log(creazioneQuadratino)
+    
+    
+    
+    
+    //inseriamo il quadrato creato dalla funzione nella griglia
+    //creo ciclo for che mi crea i 100 quadratini
+    for(let i = 0; i < 81; i++){
+            let elemento = creazioneQuadratino()
+            console.log(elemento);
+            elemento.addEventListener(`click`, function(){
+                console.log(this);
+                this.classList.toggle(`attivazione`)
+                somma = i
+                somma = somma + 1
+               console.log(somma)
+               
+        })
+        
+    
+    
+       
+        griglia.append(elemento)
+    
+    }
+    }
+}else{
+
+    function generaTre(){
+        document.getElementById(`generaLivelloUno`).style.display = "none";
+        document.getElementById(`generaLivelloDue`).style.display = "none";
+        document.getElementById(`generaLivelloTre`).style.display = "none";
+        document.getElementById(`titolo`).style.display = "none";
+        document.getElementById(`griglia`).style.display = "flex";
+    
+        let griglia = document.getElementById(`griglia`);
+        
+    }
+    
+    function creazioneQuadratino(){
+    
+     const divQ = document.createElement(`div`);
+     divQ.classList.add(`quadratoTre`);
+     
+     return divQ
+    
+    }
+    console.log(creazioneQuadratino)
+    
+    
+    //inseriamo il quadrato creato dalla funzione nella griglia
+    //creo ciclo for che mi crea i 100 quadratini
+    for(let i = 0; i < 49; i++){
+            let elemento = creazioneQuadratino()
+            console.log(elemento);
+            elemento.addEventListener(`click`, function(){
+                console.log(this);
+                this.classList.toggle(`attivazione`)
+                somma = i
+                somma = somma + 1
+               console.log(somma)
+              
+        })  
+    
+        griglia.append(elemento)
+    
+    }
+}
+})
+
+
+
 
 
 /* PRIMO LIVELLO*/
-document.getElementById(`generaLivelloUno`).onclick = function() {genera()}
-  
+/*document.getElementById(`generaLivelloUno`).onclick = function() {genera()}
 function genera(){
     document.getElementById(`generaLivelloUno`).style.display = "none";
     document.getElementById(`generaLivelloDue`).style.display = "none";
@@ -11,12 +164,6 @@ function genera(){
     document.getElementById(`titolo`).style.display = "none";
     document.getElementById(`griglia`).style.display = "flex";
 let griglia = document.getElementById(`griglia`);
-
-//console.log(griglia)
-
-//function genera(){
-    
-//}
 
 function creazioneQuadratino(){
 
@@ -28,7 +175,7 @@ function creazioneQuadratino(){
 }
 console.log(creazioneQuadratino)
 
-//somma = 0
+
 
 
 //inseriamo il quadrato creato dalla funzione nella griglia
@@ -42,27 +189,22 @@ for(let i = 0; i < 100; i++){
             somma = i
             somma = somma + 1
            console.log(somma)
-           //document.write(somma);
-
-           //elemento.classList.add(`numerazione`)
+           
     })
     
 
 
-   /* elemento.addEventListener(`click`, function(){
-        console.log(this)
-        this.classList.toggle(`numerazione`)
-    })*/
+   
     griglia.append(elemento)
 
 }
 
 }
 
-
+*/
 /* SECONDO LIVELLO */
 
-document.getElementById(`generaLivelloDue`).onclick = function() {generaDue()}
+/*document.getElementById(`generaLivelloDue`).onclick = function() {generaDue()}
 function generaDue(){
     document.getElementById(`generaLivelloUno`).style.display = "none";
     document.getElementById(`generaLivelloDue`).style.display = "none";
@@ -71,11 +213,7 @@ function generaDue(){
     document.getElementById(`griglia`).style.display = "flex";
     let griglia = document.getElementById(`griglia`);
 
-//console.log(griglia)
 
-//function genera(){
-    
-//}
 
 function creazioneQuadratino(){
 
@@ -87,7 +225,7 @@ function creazioneQuadratino(){
 }
 console.log(creazioneQuadratino)
 
-//somma = 0
+
 
 
 //inseriamo il quadrato creato dalla funzione nella griglia
@@ -101,29 +239,21 @@ for(let i = 0; i < 81; i++){
             somma = i
             somma = somma + 1
            console.log(somma)
-           //document.write(somma);
-
-           //elemento.classList.add(`numerazione`)
+           
     })
     
 
 
-   /* elemento.addEventListener(`click`, function(){
-        console.log(this)
-        this.classList.toggle(`numerazione`)
-    })*/
+   
     griglia.append(elemento)
 
 }
 }
-
+*/
 
 /* TERZO LIVELLO */
 
-
-/* SECONDO LIVELLO */
-
-document.getElementById(`generaLivelloTre`).onclick = function() {generaTre()}
+/*document.getElementById(`generaLivelloTre`).onclick = function() {generaTre()}
 function generaTre(){
     document.getElementById(`generaLivelloUno`).style.display = "none";
     document.getElementById(`generaLivelloDue`).style.display = "none";
@@ -133,11 +263,7 @@ function generaTre(){
 
     let griglia = document.getElementById(`griglia`);
     
-//console.log(griglia)
-
-//function genera(){
-    
-//}
+}
 
 function creazioneQuadratino(){
 
@@ -148,8 +274,6 @@ function creazioneQuadratino(){
 
 }
 console.log(creazioneQuadratino)
-
-//somma = 0
 
 
 //inseriamo il quadrato creato dalla funzione nella griglia
@@ -163,18 +287,10 @@ for(let i = 0; i < 49; i++){
             somma = i
             somma = somma + 1
            console.log(somma)
-           //document.write(somma);
+          
+    })  
 
-           //elemento.classList.add(`numerazione`)
-    })
-    
-
-
-   /* elemento.addEventListener(`click`, function(){
-        console.log(this)
-        this.classList.toggle(`numerazione`)
-    })*/
     griglia.append(elemento)
 
 }
-}
+*/
